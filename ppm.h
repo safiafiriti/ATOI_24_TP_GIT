18bbb848f173200be6ad9bec1ab3daea9110195b
safia <safia.firiti05@gmail.com>
2024-03-02 23:15:22 +0100

    Ajout de la documentation pour ppm.h

--- ppm.h
+++ ppm.h
@@ -2,27 +2,29 @@
 #define PPM_H
 
 struct ppm_pixel {
-  unsigned char r;
-  unsigned char g;
-  unsigned char b;
+  unsigned char r; //composant rouge du pixel
+  unsigned char g;//composant vert du pixel
+  unsigned char b;//composant bleu du pixel
 };
-
+//definission des valeurs RGB d'un pixel
 static inline void ppm_setpixel(struct ppm_pixel *px, unsigned char r,
                                 unsigned char g, unsigned char b) {
   px->r = r;
   px->g = g;
   px->b = b;
 }
-
+//structure dune image en pixel
 struct ppm_image {
-  unsigned int width;
-  unsigned int height;
-  struct ppm_pixel *px;
+  unsigned int width; //la largeur de l'image en pixel
+  unsigned int height; //la hauteur de l'image en pixel
+  struct ppm_pixel *px; //pointeur vers un tableau et pixel dune image
 };
-
-int ppm_image_init(struct ppm_image *im, int w, int h);
+//initialisation de l'mage avec les valeurs de sa largeur et hauteur
+int ppm_image_init(struct ppm_image *im, int w, int h); 
+//liberer la memoire reservée pour l'image
 int ppm_image_release(struct ppm_image *im);
 
+//definission des coordonnées d'un pixel dans une image
 static inline void ppm_image_setpixel(struct ppm_image *im, int x, int y,
                                       unsigned char r, unsigned char g,
                                       unsigned char b) {
@@ -30,6 +32,7 @@ static inline void ppm_image_setpixel(struct ppm_image *im, int x, int y,
   ppm_setpixel(px, r, g, b);
 }
 
+//sauvegarde le contenu de l'image
 int ppm_image_dump(struct ppm_image *im, char *path);
 
 #endif /* PPM_H */
